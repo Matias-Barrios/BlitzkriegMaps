1cd9d3f54220591b926c5c7b5900a0389b0b119c
Matias-Barrios <soymatiasbarrios@gmail.com>
2020-03-12 21:29:57 -0200

    Firts layout and initial scripting

--- Rats/Rats.lua
+++ Rats/Rats.lua
@@ -1,16 +1,45 @@
-function DebugView()
-	Password("Panzerklein");
-	DisplayTrace("Test 1... 2...");
-	ShowActiveScripts();
-	ChangeWarFog(1);
-	Suicide();
-end;
-
-
 function ShowText()
 	DisplayTrace("Running!");
 end;
 
 function Init()
 	RunScript("DebugView",1000);
+	-- RunScript("TraceNumberOfUnits",1000);
+	RunScript("ToLose",3000);
+	RunScript("CheckFarm1",3000);
 end;
+
+
+function CheckFarm1()
+	local total = GetNUnitsInScriptGroup(1001,1) +  GetNUnitsInScriptGroup(1002,1) +  GetNUnitsInScriptGroup(1003,1)
+	
+	Trace("Total : %g",total)
+	if (total == 0) then
+		DisplayTrace("You conquered the farm! \nWatch out. Enemies are incoming.");
+	end;
+end;
+
+-- Common functions
+
+function TraceNumberOfUnits()
+	Trace("Number of units for player 0 : %g",GetNUnitsInSide(0));
+end;
+
+function ToLose()
+	if (GetNUnitsInSide(0) == 0) then
+		DisplayTrace("You have lost!");
+		Loose();
+	end;
+end;
+
+
+function DebugView()
+	Password("Panzerklein");
+	Trace("Starting to debug...");
+	ShowActiveScripts();
+	SetGameSpeed(6);
+	ChangeWarFog(1);
+	God(0,1);
+	Suicide();
+end;
+
